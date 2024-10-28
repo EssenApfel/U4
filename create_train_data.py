ab1f80aa1618a72d581f06c5a86d727d0e4eec4c
takasago <takasago@pisces.cl.ics.tut.ac.jp>
2024-10-28 18:08:10 +0900

    ファインチューニングのための訓練データの作成を行うプログラム(未完成)

---
+++ create_train_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+path = "/home/takasago/U4/table_qa/train_tqa/sentencegen_output/output_macro_probability/S100INDK/S100INDK-0105330-tab323_sentences.csv"
+# CSVファイルの読み込み
+df = pd.read_csv(path)
